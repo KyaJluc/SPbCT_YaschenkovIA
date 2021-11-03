e7be141c64284426145083056f268db8019d0ade
Ivan Yaschenkov <91260856+KyaJluc@users.noreply.github.com>
2021-11-03 19:24:10 +0300

    Add files via upload

---
+++ lab26/zadanie/server.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#define NAME "sfifo.cc"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int fd;
+	char buf[80];
+	read(open(NAME, 0), buf, sizeof(buf));
+	system(buf);
+	close(fd);
+	unlink(NAME); return 0;
+}
